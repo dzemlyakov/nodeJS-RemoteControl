8dd5126ea5ede4776a9cd1aa0d8f1878e20baab4
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-06-22 15:49:18 +0300

    feat: complete mouse control

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
-import Jimp from 'jimp';
-import {httpServer} from './src/http_server/index.js';
-import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import Jimp from "jimp";
+import { httpServer } from "./src/http_server/index.js";
+import robot from "robotjs";
+import { WebSocketServer } from "ws";
 
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-const wss = new WebSocketServer({port:8080})
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', ws => {
-    console.log('Connection accepted!');
-    ws.on('message', data => {
-        console.log(data.toString());
-    })
-    ws.send(robot.moveMouse(100,100))
-})
+wss.on("connection", (ws) => {
+  console.log("Connection accepted!");
+  const mouseObj = {
+    up: "mouse_up",
+    down: "mouse_down",
+    left: "mouse_left",
+    right: "mouse_right",
+  };
+  const parseInput = (data) => {
+    let [direction, step] = data.split(" ");
+  };
+
+  const mouseControl = (data) => {
+    let [direction, step] = data.toString().split(" ");
+    const mousePos = robot.getMousePos();
+
+    switch (direction) {
+      case "mouse_up":
+        robot.moveMouseSmooth(mousePos.x, mousePos.y - +step);
+        ws.send(data.toString());
+        break;
+      case "mouse_down":
+        ws.send(data.toString());
+        robot.moveMouseSmooth(mousePos.x, mousePos.y + +step);
+        break;
+      case "mouse_left":
+        ws.send(data.toString());
+        robot.moveMouseSmooth(mousePos.x - +step, mousePos.y);
+        break;
+      case "mouse_right":
+        ws.send(data.toString());
+        robot.moveMouseSmooth(mousePos.x + +step, mousePos.y);
+        break;
+      default:
+        ws.send(`mouse_position ${mousePos.x},${mousePos.y}`);
+        break;
+    }
+  };
+  ws.on("message", (data) => {
+    console.log(data.toString());
+    if (data.toString().startsWith("mouse_")) {
+      mouseControl(data);
+    }
+  });
+});
